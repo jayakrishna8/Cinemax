3e4952667a8e145461f23dfac406921c7ab87a38
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-08-09 21:53:48 +0530

    Integrated payment gateway

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  # def require_authorized_for_current_lesson
+  #   if current_course.user != current_user
+  #     redirect_to course_path, alert: 'Error Message Here'
+  #   end
+  # end
 
   helper_method :current_lesson
   def current_lesson
