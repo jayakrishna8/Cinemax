7dff3d7802b9198844bb30f07155f60281f6f19c
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-08-09 23:40:44 +0530

    implemented dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
+  resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
     resources :lessons, only: [:update]
     resources :sections, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
